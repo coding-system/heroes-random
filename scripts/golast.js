f0935c51314c256da642a8de375b007134e731ec
Kirill Sheyanov <amocodingsystem@gmail.com>
2024-09-27 20:41:18 +0300

    sdfsdf

--- scripts/golast.js
+++ scripts/golast.js
@@ -1,91 +1,104 @@
-const rangeInput = document.querySelector('#rangeInput');
-const yesBox = document.querySelector('.chanses-box-yes');
-const noBox = document.querySelector('.chanses-box-no');
-const goLastSubmit = document.querySelector('.go-last__button');
-const goLastResult = document.querySelector('.go-last__result_number');
+const rangeInput = document.querySelector("#rangeInput");
+const yesBox = document.querySelector(".chanses-box-yes");
+const noBox = document.querySelector(".chanses-box-no");
+const goLastSubmit = document.querySelector(".go-last__button");
+const goLastResult = document.querySelector(".go-last__result_number");
 
 // Получаем каждый span элемент для отображения цифр
-const resultOne = document.querySelector('.go-last__result-one');
-const resultTwo = document.querySelector('.go-last__result-two');
-const resultThree = document.querySelector('.go-last__result-three');
-const resultText = document.querySelector('.go-last__result-text');
+const resultOne = document.querySelector(".go-last__result-one");
+const resultTwo = document.querySelector(".go-last__result-two");
+const resultThree = document.querySelector(".go-last__result-three");
+const resultText = document.querySelector(".go-last__result-text");
 
 export function updateRange() {
-    const yesValue = rangeInput.value;
-    const noValue = rangeInput.max - yesValue;
-    const percentage = (yesValue - rangeInput.min) / (rangeInput.max - rangeInput.min) * 100;
-    
-    // Обновляем градиент ползунка
-    rangeInput.style.background = `linear-gradient(to right, green ${percentage}%, red ${percentage}%)`;
-    
-    // Обновляем значение, выводимое на ползунке
-    rangeInput.setAttribute('yesValue', yesValue);
-    rangeInput.setAttribute('noValue', noValue);
+   const yesValue = rangeInput.value;
+   const noValue = rangeInput.max - yesValue;
+   const percentage =
+      ((yesValue - rangeInput.min) / (rangeInput.max - rangeInput.min)) * 100;
 
-    // Обновляем значения в элементах
-    yesBox.textContent = `${yesValue / 10}%`;
-    noBox.textContent = `${noValue / 10}%`;
+   // Обновляем градиент ползунка
+   rangeInput.style.background = `linear-gradient(to right, green ${percentage}%, red ${percentage}%)`;
+
+   // Обновляем значение, выводимое на ползунке
+   rangeInput.setAttribute("yesValue", yesValue);
+   rangeInput.setAttribute("noValue", noValue);
+
+   // Обновляем значения в элементах
+   yesBox.textContent = `${yesValue / 10}%`;
+   noBox.textContent = `${noValue / 10}%`;
 }
 
-rangeInput.addEventListener('input', updateRange);
+rangeInput.addEventListener("input", updateRange);
 
 export function getGolastResult() {
-   goLastSubmit.disabled = true
-   goLastResult.style.color = 'black';
-    const yesValue = rangeInput.getAttribute('yesValue');
-    const randIndex = Math.floor(Math.random() * rangeInput.max);
-    showGoLastResult(randIndex, yesValue);
-    setTimeout(() => goLastSubmit.disabled = false, 3000);
+   goLastSubmit.disabled = true;
+   goLastResult.style.color = "black";
+   const yesValue = rangeInput.getAttribute("yesValue");
+   const randIndex = Math.floor(Math.random() * rangeInput.max);
+   showGoLastResult(randIndex, yesValue);
+   setTimeout(() => (goLastSubmit.disabled = false), 3000);
 }
 
 export function showGoLastResult(randIndex, yesValue) {
-    // Преобразуем randIndex в строку с тремя символами
-    const randIndexStr = randIndex.toString().padStart(3, '0');
+   goLastResult.style.opacity = "1";
+   resultText.style.opacity = "1";
+   goLastResult.style.transform = "translateY(-100%)";
+   resultText.style.transform = "translateY(100%)";
+   // Преобразуем randIndex в строку с тремя символами
+   const randIndexStr = randIndex.toString().padStart(3, "0");
 
-    // Функция для анимации цифр, которая будет показывать случайные числа и останавливаться на целевом числе
-    function animateDigit(resultElement, targetDigit, delay) {
-        let lastDigit = -1; // Переменная для хранения последней цифры
-        let currentDigit = 0; // Начальная цифра
-        resultElement.textContent = currentDigit; // Устанавливаем стартовую цифру
-   
+   // Функция для анимации цифр, которая будет показывать случайные числа и останавливаться на целевом числе
+   function animateDigit(resultElement, targetDigit, delay) {
+      let lastDigit = -1; // Переменная для хранения последней цифры
+      let currentDigit = 0; // Начальная цифра
+      resultElement.textContent = currentDigit; // Устанавливаем стартовую цифру
 
-        const interval = setInterval(() => {
-            // Генерируем случайную цифру от 0 до 9, проверяя, что она не совпадает с последней
-            do {
-                currentDigit = Math.floor(Math.random() * 10);
-            } while (currentDigit === lastDigit); // Повторяем, пока цифра совпадает с предыдущей
+      const interval = setInterval(() => {
+         // Генерируем случайную цифру от 0 до 9, проверяя, что она не совпадает с последней
+         do {
+            currentDigit = Math.floor(Math.random() * 10);
+         } while (currentDigit === lastDigit); // Повторяем, пока цифра совпадает с предыдущей
 
-            lastDigit = currentDigit; // Обновляем последнюю цифру
-            resultElement.textContent = currentDigit; // Показываем эту цифру
-        }, 10); // Интервал смены цифр (0.1 сек)
+         lastDigit = currentDigit; // Обновляем последнюю цифру
+         resultElement.textContent = currentDigit; // Показываем эту цифру
+      }, 10); // Интервал смены цифр (0.1 сек)
 
-        // Остановка на целевом числе через заданную задержку
-        setTimeout(() => {
-            clearInterval(interval); // Останавливаем прокрутку случайных цифр
-            resultElement.textContent = targetDigit; // Устанавливаем финальное значение
-        }, delay);
-    }
+      // Остановка на целевом числе через заданную задержку
+      setTimeout(() => {
+         clearInterval(interval); // Останавливаем прокрутку случайных цифр
+         resultElement.textContent = targetDigit; // Устанавливаем финальное значение
+      }, delay);
+   }
 
+   // Запускаем анимацию для каждой цифры с разной задержкой
+   animateDigit(resultThree, randIndexStr[2], 1000); // Третий элемент останавливается через 1 секунду
+   animateDigit(resultTwo, randIndexStr[1], 2000); // Второй элемент останавливается через 2 секунды
+   animateDigit(resultOne, randIndexStr[0], 3000); // Первый элемент останавливается через 3 секунды
 
-    // Запускаем анимацию для каждой цифры с разной задержкой
-    animateDigit(resultThree, randIndexStr[2], 1000); // Третий элемент останавливается через 1 секунду
-    animateDigit(resultTwo, randIndexStr[1], 2000); // Второй элемент останавливается через 2 секунды
-    animateDigit(resultOne, randIndexStr[0], 3000); // Первый элемент останавливается через 3 секунды
-
-    // Устанавливаем цвет результата
-    if (randIndex <= yesValue) {
+   // Устанавливаем цвет результата
+   if (randIndex <= yesValue) {
       //   goLastResult.style.color = 'green';
-        setTimeout(() => {
-         goLastResult.style.color = 'green'}, 3000);
-         setTimeout(() => {
-            resultText.textContent = 'YES'}, 3000);
-    } else {
+      setTimeout(() => {
+         goLastResult.style.opacity = "0";
+         resultText.style.opacity = "1";
+         goLastResult.style.transform = "translateY(0)";
+         resultText.style.transform = "translateY(0)";
+         goLastResult.style.color = "green";
+         resultText.style.color = "green";
+         resultText.textContent = "YES";
+      }, 3000);
+   } else {
       //   goLastResult.style.color = 'red';
-        setTimeout(() => {
-         goLastResult.style.color = 'red'}, 3000);
-         setTimeout(() => {
-            resultText.textContent = 'NO'}, 3000);
-    }
+      setTimeout(() => {
+         goLastResult.style.opacity = "0";
+         resultText.style.opacity = "1";
+         goLastResult.style.transform = "translateY(0)";
+         resultText.style.transform = "translateY(0)";
+         goLastResult.style.color = "red";
+         resultText.style.color = "red";
+         resultText.textContent = "NO";
+      }, 3000);
+   }
 }
 
-goLastSubmit.addEventListener('click', getGolastResult);
+goLastSubmit.addEventListener("click", getGolastResult);
